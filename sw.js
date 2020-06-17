f2c901f87763a47681a2459cf89b84eab4af6beb
Michael Lawrence <me@mikelawrence.co>
2020-06-17 16:06:02 -0400

    Updating service worker.

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ self.addEventListener('install', e => e.waitUntil(
         ]))
 ));
 
-self.addEventListener('fetch', e => console.log(e.request.url) && e.respondWith(
+self.addEventListener('fetch', e => e.respondWith(
     caches
         .match(e.request)
         .then(response => response || fetch(e.request))
